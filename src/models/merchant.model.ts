3b44dcb0ada0076b16935fbfa99d31bc695b9628
ibrahim issa basha <94835822+PASHA011@users.noreply.github.com>
2023-05-29 13:45:02 +0300

    #13

---
+++ src/models/merchant.model.ts
@@ -0,0 +1,11 @@
+export class Merchant{
+    id:string;
+    title:string;
+    genericListId: string;
+    foundingDate: Date;
+    taxOffice: string;
+    taxNumber: string;
+    phone: string;
+    website: string;
+
+}
