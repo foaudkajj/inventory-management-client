25239e7e7677a773389853e8281f2b262d874027
ibrahim issa basha <94835822+PASHA011@users.noreply.github.com>
2023-08-24 10:10:40 +0300

    #50

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 import "./footer.scss";
+import { useNavigation } from "../../contexts/navigation";
+import { useFooter } from "../../contexts/footer";
 
 export default ({ ...rest }) => {
-  return <footer className={"footer"} {...rest} />;
+  const { navigationData } = useNavigation();
+  const { displayed, trigger } = useFooter();
+  if (navigationData.currentPath === "/sellingPage") {
+    trigger(false);
+  } else {
+    trigger(true);
+  }
+  if (displayed) { return <footer className={"footer"} {...rest} />; }
+
 };
